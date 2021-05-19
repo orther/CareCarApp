b47d2d5aef77f78841aed963298e29605a5907de
Brandon Orther <brandon@orther.dev>
2021-05-18 23:23:23 -0700

    Add NativeWebView component

---
+++ app/components/native-webview/native-webview.tsx
@@ -0,0 +1,19 @@
+import React, { CSSProperties } from "react"
+import { Platform } from "react-native"
+import { WebView } from "react-native-webview"
+
+interface NativeWebViewProps {
+  target: string
+}
+
+export const NativeWebView = (props: NativeWebViewProps): JSX.Element => {
+  // if (Platform.OS === "web") {
+  //   return <iframe src={props.target} style={styles} />
+  // }
+  return <WebView source={{ uri: props.target }} />
+}
+
+const styles: CSSProperties = {
+  height: 600,
+  width: 800,
+}
